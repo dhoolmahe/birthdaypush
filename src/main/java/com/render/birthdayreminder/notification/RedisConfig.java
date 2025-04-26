02aea2ceb94a8d60ef88aa01cd6c3b366cec4756
dhoolmahe <mahendran.visvanathan@gmail.com>
2025-04-26 08:19:31 +0200

    modified redis config

--- src/main/java/com/render/birthdayreminder/notification/RedisConfig.java
+++ src/main/java/com/render/birthdayreminder/notification/RedisConfig.java
@@ -4,35 +4,41 @@ import io.lettuce.core.RedisURI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-
-import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
-
     @Value("${redis.url}")
     private String redisUrl;
 
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Value("${redis.password}")
+    private String redisPassword;
+
     @Bean
-    public ReactiveRedisConnectionFactory redisConnectionFactory() {
-        RedisURI uri = RedisURI.create(redisUrl);
-        return new LettuceConnectionFactory((RedisConfiguration) uri);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisUrl);
+        config.setPort(redisPort);
+        config.setPassword(redisPassword);
+        return new LettuceConnectionFactory(config);
     }
 
 //    @Bean
-//    public ReactiveStringRedisTemplate redisTemplate(ReactiveRedisConnectionFactory factory) {
+//    public ReactiveStringRedisTemplate redisTemplate(LettuceConnectionFactory factory) {
 //        return new ReactiveStringRedisTemplate(factory);
 //    }
 
     @Bean
-    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
-            ReactiveRedisConnectionFactory factory) {
-        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
     }
 }
-
