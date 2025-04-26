015057d79a50f21844d5027cc1d211230e0c7946
dhoolmahe <mahendran.visvanathan@gmail.com>
2025-04-26 07:43:31 +0200

    Added Redis

--- src/main/java/com/render/birthdayreminder/notification/RedisConfig.java
+++ src/main/java/com/render/birthdayreminder/notification/RedisConfig.java
@@ -1,22 +1,38 @@
 package com.render.birthdayreminder.notification;
 
+import io.lettuce.core.RedisURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public ReactiveRedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("your-upstash-host", 6379);
-    }
+
+    @Value("${redis.url}")
+    private String redisUrl;
 
     @Bean
-    public ReactiveStringRedisTemplate redisTemplate(ReactiveRedisConnectionFactory factory) {
-        return new ReactiveStringRedisTemplate(factory);
+    public ReactiveRedisConnectionFactory redisConnectionFactory() {
+        RedisURI uri = RedisURI.create(redisUrl);
+        return new LettuceConnectionFactory((RedisConfiguration) uri);
+    }
+
+//    @Bean
+//    public ReactiveStringRedisTemplate redisTemplate(ReactiveRedisConnectionFactory factory) {
+//        return new ReactiveStringRedisTemplate(factory);
+//    }
+
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
+            ReactiveRedisConnectionFactory factory) {
+        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
     }
 }
 
